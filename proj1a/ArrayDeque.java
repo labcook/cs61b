b1c7b2a7675c00b887092f246b470b1f85ea3ad1
Hamilton Chen <dour@163.com>
2020-12-10 10:57:24 +0800

    4th try based on someone's resize method

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -8,42 +8,29 @@ public class ArrayDeque<T> {
     public ArrayDeque() {
         items = (T []) new Object[8];
         size = 0;
-        nextFirst = 0;
-        nextLast = 1;
+        int mid = items.length/2;
+        nextFirst = mid;
+        nextLast = mid+1;
     }
 
-    private void addResize(int capacity) {
+    private void resize(int capacity) {
         T[] a = (T []) new Object[capacity];
-        // hard to imagine
-        int rem = size-(nextFirst+1)%items.length;
-        System.arraycopy(items, (nextFirst+1)%items.length, a, 0, rem);
-        System.arraycopy(items, 0, a, rem, size-rem);
-        items = a;
-        nextLast = size;
-        nextFirst = items.length-1;
-    }
-
-    private void removeResize(int capacity) {
-        T[] a = (T []) new Object[capacity];
-        // hard to imagine
-        if(nextLast > nextFirst){
-            System.arraycopy(items, (nextFirst+1)%items.length, a, 0, size);
-            items = a;
-            nextLast = size;
-            nextFirst = items.length-1;
-        }else{
-            int rem = items.length-(nextFirst+1);
-            System.arraycopy(items, (nextFirst+1)%items.length, a, 0, rem);
-            System.arraycopy(items, 0, a, rem, size-rem);
-            items = a;
-            nextLast = size;
-            nextFirst = items.length-1;
+        int startPos = a.length/2 - size/2;
+        int oldIndex = nextFirst + 1;
+        int newIndex = startPos;
+        int count = 0;
+        while(count < size){
+            a[newIndex] = items[oldIndex % items.length];
+            oldIndex++; newIndex++;count++;
         }
+        items = a;
+        nextFirst = startPos -1;
+        nextLast = newIndex;
     }
 
     public void addLast(T item) {
         if (size == items.length) {
-            addResize(size * 2);
+            resize(size * 2);
         }
         items[nextLast] = item;
         size += 1;
@@ -52,11 +39,12 @@ public class ArrayDeque<T> {
 
     public void addFirst(T item) {
         if (size == items.length) {
-            addResize(size * 2);
+            resize(size * 2);
         }
         items[nextFirst] = item;
         size += 1;
-        nextFirst = (nextFirst-1) % items.length;
+        //must add items.length
+        nextFirst = (nextFirst-1+items.length) % items.length;
     }
 
     public boolean isEmpty() {
@@ -72,35 +60,57 @@ public class ArrayDeque<T> {
 
     public void printDeque(){
         if(size == 0) return;
-        for(int i =1 ; i <= size; i++){
-            System.out.print(items[(nextFirst+i)% items.length] + " ");
+        int oldIndex = nextFirst + 1;
+        for(int i =0 ; i < size; i++){
+            System.out.print(items[(oldIndex+i)% items.length] + " ");
         }
     }
 
     public T removeFirst() {
         if (size == 0) return null;
-        T res = items[(nextFirst+1)%items.length];
-        size -= 1;
-        nextFirst = (nextFirst+1)%items.length;
-        if(size < items.length/4){
-            removeResize(size/2);
+        if((double)size / items.length < 0.25 && items.length > 16){
+            resize(size/2);
         }
+        nextFirst = (nextFirst+1)%items.length;
+        T res = items[nextFirst];
+        size -= 1;
         return  res;
     }
 
     public T removeLast() {
         if (size == 0) return null;
-        T res = items[(nextLast-1)%items.length];
-        size -= 1;
-        nextLast = (nextLast-1)%items.length;
-        if(size < items.length/4){
-            removeResize(size/2);
+        if((double)size / items.length < 0.25 && items.length > 16){
+            resize(size/2);
         }
+        nextLast = (nextLast-1+items.length)%items.length;
+        T res = items[nextLast];
+        items[nextLast] = null;
+        size -= 1;
         return  res;
     }
 
     public T get(int index) {
         if(index < 0 || index + 1 > size) return null;
-        return items[nextFirst+1+index];
+        int oldIndex = nextFirst + 1;
+        while(index > 0){
+            oldIndex++;
+            index--;
+        }
+        return items[oldIndex%items.length];
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque test = new ArrayDeque();
+        test.addFirst(6);
+        test.addFirst(5);
+        test.addFirst(4);
+        test.addFirst(3);
+        test.addFirst(2);
+        test.addFirst(1);
+        test.addLast(7);
+        test.addLast(8);
+        test.addLast(9);
+        System.out.print(test.size);
+        test.printDeque();
     }
 }
